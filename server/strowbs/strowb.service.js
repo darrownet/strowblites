04a5cc3dca0ab227117eb374199ace1fdc472cb0
Matt Schiffman <>
2021-02-03 09:24:43 -0500

    is a workin

--- server/strowbs/strowb.service.js
+++ server/strowbs/strowb.service.js
@@ -5,6 +5,7 @@ const crypto = require("crypto");
 const sendEmail = require('_helpers/send-email');
 const db = require('_helpers/db');
 const Role = require('_helpers/role');
+const aws = require('aws-sdk');
 
 const PassThrough = require('stream').PassThrough;
 
@@ -22,6 +23,42 @@ module.exports = {
     // delete: _delete
     // getAll,
     // getById
+};
+
+
+const uploadToS3Bucket = (image, strowb) => {
+    console.log(strowb);
+    return new Promise((resolve, reject) => {
+        const s3 = new aws.S3({
+            signatureVersion: 'v4',
+            region: 'us-east-1',
+            accessKeyId: 'AKIAJF2IEYUJAVOQWAUQ',
+            secretAccessKey: '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'
+        });
+        const params = {
+            'ACL': 'public-read',
+            'Bucket': 'strowblites',
+            'Key': `${strowb.id}/strowb-${strowb.id}.webp`,
+            'Body': image,
+            'ContentType': "image/webp",
+            'x-amz-tagging': strowb.tags?.reduce(()=>{}, '') || '',
+            'Metadata': {
+                'type': 'webp',
+                'id': strowb.id,
+                'title': strowb?.title || '',
+                'caption1': strowb.frame1?.caption || '',
+                'caption2': strowb.frame2?.caption || '',
+                'user-id': strowb.userId
+            }
+        };
+        s3.putObject(params, function (err, data) {
+            if (err) {
+                console.log(err);
+            } else {
+                resolve();
+            }
+        });
+    });
 };
 
 function basicDetails(strowb) {
@@ -57,53 +94,122 @@ async function create(params) {
         fs.mkdirSync(dir);
     }
 
-    const img1FileData = params.frame1.image.substr(params.frame1.image.indexOf(',') + 1);
-    const img2FileData = params.frame2.image.substr(params.frame2.image.indexOf(',') + 1);
+    // const img1FileData = params.frame1.image.substr(params.frame1.image.indexOf(',') + 1);
+    // const img2FileData = params.frame2.image.substr(params.frame2.image.indexOf(',') + 1);
 
-    gm(new Buffer.from(img1FileData, 'base64'))
-        .toBuffer('webp', (err, buffer1) => {
-            fs.writeFile(`${dir}/frame1-${strowb.id}.webp`, buffer1, function (err) {
-                if(err) throw err.message;
-                console.log("IMG1 ORIG SAVED!");
+    // gm(new Buffer.from(img1FileData, 'base64'))
+    //     .toBuffer('webp', (err, buffer1) => {
+    //         fs.writeFile(`${dir}/frame1-${strowb.id}.webp`, buffer1, function (err) {
+    //             if (err) throw err.message;
+    //             console.log("IMG1 ORIG SAVED!");
+    //
+    //             gm(new Buffer.from(img2FileData, 'base64'))
+    //                 .toBuffer('webp', function (err, buffer2) {
+    //                     fs.writeFile(`${dir}/frame2-${strowb.id}.webp`, buffer2, function (err) {
+    //                         if (err) throw err.message;
+    //                         console.log("IMG2 ORIG SAVED!");
+    //
+    //                         const input = [
+    //                             {"path": `${dir}/frame1-${strowb.id}.webp`, "offset": `+${params.frame1.delay}`},
+    //                             {"path": `${dir}/frame2-${strowb.id}.webp`, "offset": `+${params.frame1.delay}`}
+    //                         ];
+    //
+    //                         const result = webp.webpmux_animate(input, `${dir}/strowb-${strowb.id}.webp`, "0", "255,255,255,255");
+    //                         result.then((response) => {
+    //                             console.log(response);
+    //                             // upload ${dir} to S3
+    //                             // then...
+    //                             const updateObj = {
+    //                                 frame1: {
+    //                                     caption: params.frame1.caption,
+    //                                     delay: params.frame1.delay,
+    //                                     image: `frame1-${strowb.id}.webp`,
+    //                                     style: params.frame1.style
+    //                                 },
+    //                                 frame2: {
+    //                                     caption: params.frame2.caption,
+    //                                     delay: params.frame1.delay,
+    //                                     image: `frame2-${strowb.id}.webp`,
+    //                                     style: params.frame2.style
+    //                                 },
+    //                                 strowb: `strowb-${strowb.id}.webp`
+    //                             }
+    //                             db.Strowb.findByIdAndUpdate(strowb.id, updateObj, {new: true}, () => {
+    //                                 console.log();
+    //                             });
+    //                         });
+    //
+    //                     })
+    //                 })
+    //         })
+    //     })
 
-                gm(new Buffer.from(img2FileData, 'base64'))
-                    .toBuffer('webp', function (err, buffer2) {
-                        fs.writeFile(`${dir}/frame2-${strowb.id}.webp`, buffer2, function (err) {
-                            if(err) throw err.message;
-                            console.log("IMG2 ORIG SAVED!");
+    // gm(new Buffer(img1FileData)).toBuffer('webp', (err, buffer) => {
+    //     console.log(buffer);
+    //     fs.writeFile(`${dir}/frame1-${strowb.id}.webp`, buffer, function (err) {
+    //         console.log('che');
+    //     });
+    // });
 
+    // fs.writeFile(dir, new Buffer(img2FileData, 'base64'), () => {
+    //     console.log(new Buffer(img2FileData, 'base64'));
+    // });
+
+    const inputStr1 = params.frame1.image;
+    const inputStr2 = params.frame2.image;
+
+
+    const imageBuff1 = Buffer.from(inputStr1.split(";base64,")[1], 'base64');
+    const imageBuff2 = Buffer.from(inputStr2.split(";base64,")[1], 'base64');
+
+    gm(imageBuff1)
+        .write(`${dir}/frame1-${strowb.id}.webp`, function (err1) {
+            if (!err1) {
+                gm(imageBuff2)
+                    .write(`${dir}/frame2-${strowb.id}.webp`, function (err2) {
+                        if (!err2) {
                             const input = [
-                                {"path":`${dir}/frame1-${strowb.id}.webp`, "offset":`+${params.frame1.delay}`},
-                                {"path":`${dir}/frame2-${strowb.id}.webp`, "offset":`+${params.frame1.delay}`}
+                                {"path": `${dir}/frame1-${strowb.id}.webp`, "offset": `+${params.frame1.delay}`},
+                                {"path": `${dir}/frame2-${strowb.id}.webp`, "offset": `+${params.frame2.delay}`}
                             ];
-
-                            const result = webp.webpmux_animate(input,`${dir}/strowb-${strowb.id}.webp`,"0","255,255,255,255");
-                            result.then((response) => {
-                                console.log(response);
-                                const updateObj = {
-                                    frame1: {
-                                        caption: params.frame1.caption,
-                                        delay: params.frame1.delay,
-                                        image: `frame1-${strowb.id}.webp`,
-                                        style: params.frame1.style
-                                    },
-                                    frame2: {
-                                        caption: params.frame2.caption,
-                                        delay: params.frame1.delay,
-                                        image: `frame2-${strowb.id}.webp`,
-                                        style: params.frame2.style
-                                    },
-                                    strowb: `strowb-${strowb.id}.webp`
-                                }
-                                db.Strowb.findByIdAndUpdate(strowb.id, updateObj, {new: true}, () => {
-                                    console.log();
+                            webp.webpmux_animate(
+                                input,
+                                `${dir}/strowb-${strowb.id}.webp`,
+                                "0",
+                                "255,255,255,255"
+                            ).then((response) => {
+                                const s3FilePath = fs.readFileSync(`./${dir}/strowb-${strowb.id}.webp`);
+                                uploadToS3Bucket(s3FilePath, strowb.id).then(() => {
+                                    const updateObj = {
+                                        frame1: {
+                                            caption: params.frame1.caption,
+                                            delay: params.frame1.delay,
+                                            image: `frame1-${strowb.id}.webp`,
+                                            style: params.frame1.style
+                                        },
+                                        frame2: {
+                                            caption: params.frame2.caption,
+                                            delay: params.frame2.delay,
+                                            image: `frame2-${strowb.id}.webp`,
+                                            style: params.frame2.style
+                                        },
+                                        strowb: `strowb-${strowb.id}.webp`
+                                    }
+                                    db.Strowb.findByIdAndUpdate(
+                                        strowb.id, updateObj,
+                                        {new: true},
+                                        () => {}
+                                    );
                                 });
                             });
-
-                        })
-                    })
-            })
-        })
+                        } else {
+                            console.log(err2.log, err2.stack);
+                        }
+                    });
+            } else {
+                console.log(err1.log, err1.stack);
+            }
+        });
 
     return basicDetails(strowb);
 }
